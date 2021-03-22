88ebf723ab783f44a8a856abe4238c642f859045
shashikiran2002 <79633261+shashikiran2002@users.noreply.github.com>
2021-03-22 13:13:15 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,8 @@
+class first{
+    public static void main(String args[]){
+    int a,b,c;
+    System.out.println("Hello java");
+    System.out.println("This is my april-release code");
+    
+    }
+}
