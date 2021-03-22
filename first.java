1e51c0aea5b1066e027f51e3672190316ff66b05
shashi kiran <shashikiran200212@gmail.com>
2021-03-22 13:25:09 +0530

    changes made to April release

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ class first{
     public static void main(String args[]){
     int a,b,c;
     System.out.println("Hello java");
-    System.out.println("This is my april-release code");
-    
+    System.out.println("This is April-release code");
+  
     }
 }
